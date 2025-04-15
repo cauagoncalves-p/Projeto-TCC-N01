12c4ea7642d4d6a40bcc650478ef68922df8177a
CAUA GONCALVES PEREIRA <caua.gpereira2@senacsp.edu.br>
2025-04-14 22:13:36 -0300

    criando a pagina de paciente

--- Avalia +/FormularioCadastro.Designer.cs
+++ Avalia +/FormularioCadastro.Designer.cs
@@ -109,7 +109,6 @@
             this.panelCadastro.Padding = new System.Windows.Forms.Padding(0, 0, 20, 20);
             this.panelCadastro.Size = new System.Drawing.Size(719, 788);
             this.panelCadastro.TabIndex = 0;
-          
             // 
             // lblError
             // 
--- Avalia +/Program.cs
+++ Avalia +/Program.cs
@@ -16,7 +16,7 @@ namespace Avalia__
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormInicial());
+            Application.Run(new FormularioPaginaPaciente());
         }
     }
 }
