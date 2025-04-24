afb5400d6e11932d30c5fe833fbd245d69e9db3f
CAUA GONCALVES PEREIRA <caua.gpereira2@senacsp.edu.br>
2025-04-23 22:23:45 -0300

    fazendo pagina de cancelar consulta

--- Avalia +/frmConfirmarCancelamento.Designer.cs
+++ Avalia +/frmConfirmarCancelamento.Designer.cs
@@ -28,26 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblinfo = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConfirmarCancelamento));
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblsubtitulo = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // lblinfo
+            // btnSair
             // 
-            this.lblinfo.AutoSize = true;
-            this.lblinfo.Location = new System.Drawing.Point(145, 163);
-            this.lblinfo.Name = "lblinfo";
-            this.lblinfo.Size = new System.Drawing.Size(35, 13);
-            this.lblinfo.TabIndex = 0;
-            this.lblinfo.Text = "label1";
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.Location = new System.Drawing.Point(832, 41);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(50, 43);
+            this.btnSair.TabIndex = 15;
+            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // lblsubtitulo
+            // 
+            this.lblsubtitulo.AutoSize = true;
+            this.lblsubtitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblsubtitulo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsubtitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(94)))), ((int)(((byte)(60)))));
+            this.lblsubtitulo.Location = new System.Drawing.Point(76, 75);
+            this.lblsubtitulo.Name = "lblsubtitulo";
+            this.lblsubtitulo.Size = new System.Drawing.Size(187, 22);
+            this.lblsubtitulo.TabIndex = 14;
+            this.lblsubtitulo.Text = "Agende sua consulta";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitulo.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblTitulo.Location = new System.Drawing.Point(43, 40);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(82, 24);
+            this.lblTitulo.TabIndex = 13;
+            this.lblTitulo.Text = "Áurea+";
             // 
             // frmConfirmarCancelamento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblinfo);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.lblsubtitulo);
+            this.Controls.Add(this.lblTitulo);
             this.Name = "frmConfirmarCancelamento";
-            this.Text = "frmConfirmarCancelamento";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cancelar consulta";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,6 +90,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblinfo;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblsubtitulo;
+        private System.Windows.Forms.Label lblTitulo;
     }
 }
